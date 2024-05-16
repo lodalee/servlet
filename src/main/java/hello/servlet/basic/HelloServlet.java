24457cc7673c4e2a72cd459fc6cd2345fbcde4a1
chaewon <cwlee4633@naver.com>
2024-05-17 00:50:27 +0900

    동작하는 서블릿 코드 작성하기 - 1

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,20 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import javax.sql.rowset.serial.SerialException;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+    }
+}
